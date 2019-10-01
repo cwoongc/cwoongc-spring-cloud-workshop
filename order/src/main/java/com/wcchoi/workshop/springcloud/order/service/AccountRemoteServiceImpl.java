e74642651f01d1c5378cf310116814e1c119cf7b
Woong-Chul, Choi <switch.viewpoint@gmail.com>
2019-10-02 00:02:10 +0900

    Finish hystrix.

--- order/src/main/java/com/wcchoi/workshop/springcloud/order/service/AccountRemoteServiceImpl.java
+++ order/src/main/java/com/wcchoi/workshop/springcloud/order/service/AccountRemoteServiceImpl.java
@@ -1,6 +1,7 @@
 package com.wcchoi.workshop.springcloud.order.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +14,36 @@ public class AccountRemoteServiceImpl implements AccountRemoteService {
     @Autowired private RestTemplate restTemplate;
 
 
-    @HystrixCommand(fallbackMethod = "getOrderAccountsFallback")
+    /**
+     * https://github.com/Netflix/Hystrix/wiki/Configuration
+     * @param orderNo
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "getOrderAccountsFallback",
+            // Declare circuit & circuit id. (default: method name)
+            commandKey = "getOrderAccounts",
+
+            //Assign (or declare & assign) thread pool to this DependencyCommand by thread pool name (default: class name)
+            groupKey = "getOrderAccountsThreads",
+
+            //Configuration for execution.isolation, fallback, circuitBreaker, metrics
+            commandProperties = {
+                @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500")
+            },
+
+            threadPoolProperties = {
+                @HystrixProperty(name = "allowMaximumSizeToDivergeFromCoreSize", value = "true"),
+                @HystrixProperty(name = "maximumSize", value = "20"),
+                @HystrixProperty(name = "coreSize", value = "10"),
+                @HystrixProperty(name = "keepAliveTimeInMinutes", value = "2"),
+
+                @HystrixProperty(name = "queueSizeRejectionThreshold", value = "15"),
+                @HystrixProperty(name = "maxQueueSize", value = "101"),
+
+                @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "10000"),
+                @HystrixProperty(name = "metrics.rollingStats.numBuckets", value = "10")
+            }
+    )
     @Override
     public String getOrderAccounts(Long orderNo) {
 
